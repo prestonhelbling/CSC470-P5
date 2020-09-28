c90666439c3fb41237976e680d258058dc317acd
Preston Helbling <preston.helbling@trojans.dsu.edu>
2020-09-27 22:47:52 -0500

    -Copied over Data from P3. -Made all corrections per the points lost on P3      -Recreated both unit tests & Implemented.      -Change ambigious ignore to nameUserNameExists      -Added Program Class with main function.      -Added Blank formss for P4.

---
+++ P4 Code/P4 Code/IAppUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P4_Code
+{
+    //IAppUser Interface
+    interface IAppUserRepository
+    {
+        bool Login(string UserName, string Password);
+        List<AppUser> GetAll();
+        void SetAuthentication(string UserName, bool IsAuthenticated);
+        AppUser GetByUserName(string UserName);
+    }
+}
---
+++ P4 Code/P4 Code/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P4_Code
+{
+    static class Program
+    {
+        static void Main()
+        {
+
+
+        }
+    }
+}
