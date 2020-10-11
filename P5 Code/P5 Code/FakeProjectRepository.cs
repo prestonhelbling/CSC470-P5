fc5232c23dbf55b8fecc112987deaf738bf4db6f
Preston Helbling <preston.helbling@trojans.dsu.edu>
2020-10-10 21:43:08 -0500

    -Added functionality for IProjectRepostiory interface -Added Project Class -Added FakeProjectRepository and all functions except for Modify.

---
+++ P5 Code/P5 Code/FakeProjectRepository.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P5_Code
+{
+    class FakeProjectRepository : IProjectRepository
+    {
+        public const string NO_ERROR = "";
+        public const string MODIFIED_PROJECT_ID_ERROR = "Can not modify the project id.";
+        public const string DUPLICATE_PROJECT_NAME_ERROR = "Project name already exists.";
+        public const string NO_PROJECT_FOUND_ERROR = "No project found.";
+        public const string EMPTY_PROJECT_NAME_ERROR = "Project name is empty or blank.";
+
+        private static List<Project> projects;
+        
+        public FakeProjectRepository()
+        {
+            // Default projects on creation of class. 
+            if(projects == null)
+            {
+
+                Project defaultProjectOne = new Project();
+                defaultProjectOne.name = "Town House";
+
+                Add(defaultProjectOne);
+
+                Project defaultProjectTwo = new Project();
+                defaultProjectTwo.name = "Adobe House";
+
+                projects.Add(defaultProjectTwo);
+
+                Project defaultProjectThree = new Project();
+                defaultProjectThree.name = "Large Fence";
+
+                projects.Add(defaultProjectThree);
+            }
+        }
+
+        public string Add(Project project, out int Id)
+        {
+            // Removes leaving and trailing spaces. 
+            project.name = project.name.Trim(' ');
+            // Empty Project Name
+            if(project.name.Equals(""))
+            {
+                Id = -1;
+                return EMPTY_PROJECT_NAME_ERROR;
+            }
+            // Duplicate project name.
+            else if(IsDuplicateName(project.name))
+            {
+                Id = -1;
+                return DUPLICATE_PROJECT_NAME_ERROR;
+            }
+            // Succesfully add the project. 
+            else
+            {
+                project.id = GetNextId();
+                projects.Add(project);
+            }
+        }
+
+        public string Remove(int projectId)
+        {
+            // Loop through the projects and compare the id's. If we find that id remove it. 
+            foreach (Project project in projects)
+            {
+                if (project.id == projectId)
+                {
+                    projects.RemoveAt(projectId + 1);
+                    return NO_ERROR;
+                }
+            }
+            // Failed to find project with ID.
+            return NO_PROJECT_FOUND_ERROR;
+        }
+        public string Modify(int projectId, Project project)
+        {
+            if()
+            {
+
+            }
+        }
+
+        public List<Project> GetAll()
+        {
+            return projects;
+        }
+        public bool IsDuplicateName(string projectName)
+        {
+            // Loop through the projects and compare the names. If we find a duplicate return true.
+            foreach(Project project in projects)
+            {
+                if(project.name.Equals(projectName))
+                {
+                    return true;
+                }
+            }
+            // No duplicate was found.
+            return false;
+        }
+
+        private int GetNextId()
+        {
+            Project lastProjectInList = projects.LastOrDefault<Project>();
+
+            // Found a project. 
+            if (lastProjectInList != null)
+            {
+                int newId = lastProjectInList.id + 1;
+                return newId;
+            }
+            // List is empty. 
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
---
+++ P5 Code/P5 Code/Project.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P5_Code
+{
+    class Project
+    {
+        public int id;
+        public string name;
+    }
+
+    
+}
